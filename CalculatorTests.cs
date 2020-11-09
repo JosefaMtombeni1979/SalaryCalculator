8e64ec3fce036c38167ab397fb554fba66c946af
josefa <josefa@fireworkx.co.za>
2020-11-10 00:55:30 +0200

    Add project files.

---
+++ CalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalaryCalculatorTestPrject
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        /* To get the annual salary test*/
+
+
+        [TestMethod]
+        public void AnnualSalaryTest()
+        {
+
+            //Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+
+            //Act
+            decimal annualSalary = sc.GetAnnualSalary(50);
+
+            //Assert
+            Assert.AreEqual(104000, annualSalary);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SalaryCalculatorTestPrject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SalaryCalculatorTestPrject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21e07086-e8cd-49c4-ae6c-5c1eaf6de6f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
